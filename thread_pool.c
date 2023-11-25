f94eee159d8c1076929c1315872019d4a14f249e
wenliang <wenliang@163.com>
2023-11-25 23:48:10 +0800

    [修改] 调整destroy函数结构，注意释放线程资源

--- thread_pool.c
+++ thread_pool.c
@@ -27,28 +27,26 @@ typedef struct t_task
 typedef struct thread_pool
 {
     pthread_t *threadId;                /* 动态数组 */
-    pthread_mutex_t mutex_lock;         /* 互斥锁*/
+    pthread_mutex_t mutex_lock;         /* 互斥锁1: 用于互斥共享资源 */
+    pthread_mutex_t mutex_busythrcnt_lock;         /* 互斥锁2: 用来互斥忙碌的线程数量 目的:提升效率 */
     pthread_cond_t mutex_cond_ptoc;     /* 条件变量 */
     pthread_cond_t mutex_cond_ctop;     /* 条件变量 */
 
-
-    /* 队列 */
     t_task * queue;                     /* 任务队列 */
     int     task_size;                  /* 任务数 */
 
-    /* 忙碌线程数量 */
-    int busy_thread;
-
-    /* 最大、最小线程数量 */
-    int max_thread_num;
+    /* 最小、最大线程数量 */
     int min_thread_num;
-
+    int max_thread_num;
+    /* 忙碌线程数量 */
+    int busy_thread_num;
     /* 当前线程数量 */
     int current_thread_num;
 
     /* 管理者线程 */
-    pthread_t *thread_manager;
-
+    pthread_t manager_thread;
+    
+    int shutdown;                   /* 使能关闭开关 */
 } thread_pool;
 
 /* 状态码 */
@@ -61,6 +59,11 @@ typedef enum STAT_CODE
 
 /* 函数前置声明 */
 int threadPool_Init(thread_pool **pool, int threadNums,int min_thread_num,int max_thread_num);
+/* 线程池的资源回收 */
+int threadPool_ResourceFree(thread_pool *pool);
+/* 线程池的内存释放 */
+int threadPool_Free(thread_pool *pool);
+
 
 /* 我是消费者 */
 void * thread_func(void *arg)
@@ -71,31 +74,40 @@ void * thread_func(void *arg)
     t_task *task = (t_task *)malloc(sizeof(t_task) * 1);
     memset(task, 0, sizeof(t_task));
 
-    pthread_mutex_lock(&(tpool->mutex_lock));
-    while (tpool->task_size == 0)
+    while (1)
     {
-        pthread_cond_wait(&(tpool->mutex_cond_ptoc), &(tpool->mutex_lock));
+        pthread_mutex_lock(&(tpool->mutex_lock));
+        while (tpool->task_size == 0 && !tpool->shutdown)
+        {
+            pthread_cond_wait(&(tpool->mutex_cond_ptoc), &(tpool->mutex_lock));
+        }
+
+        /* */
+        if (tpool->shutdown)
+        {
+            printf("Workers'thread ID 0x%x is exiting\n", (unsigned int)pthread_self());
+            pthread_mutex_unlock(&(tpool->mutex_lock));
+            pthread_exit(NULL);
+        }
+
+        task = tpool->queue->next;      /* 这就是我的任务 */
+        tpool->queue->next = task->next;
+        tpool->task_size--;
+        printf("pthread_self get it id:%ld\n", pthread_self());
+        pthread_mutex_unlock(&(tpool->mutex_lock));
+        pthread_cond_broadcast(&(tpool->mutex_cond_ctop));
+
+        pthread_mutex_lock(&(tpool->mutex_busythrcnt_lock));
+        tpool->busy_thread_num++;
+        pthread_mutex_unlock(&(tpool->mutex_busythrcnt_lock));
+
+        /* 执行回调函数 */
+        task->task_callback(task->arg);
+
+        pthread_mutex_lock(&(tpool->mutex_busythrcnt_lock));
+        tpool->busy_thread_num--;
+        pthread_mutex_unlock(&(tpool->mutex_busythrcnt_lock));
     }
-    task = tpool->queue->next;      /* 这就是我的任务 */
-
-
-    tpool->queue->next = task->next;
-    tpool->task_size--;
-    printf("pthread_self get it id:%ld\n", pthread_self());
-    pthread_mutex_unlock(&(tpool->mutex_lock));
-    pthread_cond_broadcast(&(tpool->mutex_cond_ctop));
-
-    pthread_mutex_lock(&(tpool->mutex_lock));
-    tpool->busy_thread++;
-    pthread_mutex_unlock(&(tpool->mutex_lock));
-
-    /* 执行回调函数 */
-    task->task_callback(task->arg);
-
-    pthread_mutex_lock(&(tpool->mutex_lock));
-    tpool->busy_thread--;
-    pthread_mutex_unlock(&(tpool->mutex_lock));
-
     pthread_exit(NULL);
 }
 
@@ -119,16 +131,14 @@ void * manager_func(void *arg)
         sleep(5);
         pthread_mutex_lock(&t_pool->mutex_lock);
         
-        int busy_thread = t_pool->busy_thread;
+        int busy_thread_num = t_pool->busy_thread_num;
         int task_num = t_pool->task_size;
         int current_thread_num = t_pool->current_thread_num;
         int max_thread_num = t_pool->max_thread_num;
         int min_thread_num = t_pool->min_thread_num;
 
-        
-
         /* 增线程 */
-        if(busy_thread * 2 < task_num  && current_thread_num < max_thread_num)
+        if(busy_thread_num * 2 < task_num  && current_thread_num < max_thread_num)
         {
             int add_thread_num  = (max_thread_num - current_thread_num) / 2;
             for(int idx = 0 ; idx < add_thread_num ; idx++)
@@ -141,12 +151,10 @@ void * manager_func(void *arg)
         }
 
         /* 减线程 */
-        if(busy_thread > task_num * 2 && current_thread_num > min_thread_num)
-        {
-            int dl_thread_num = 
-        }
-
-
+        // if(busy_thread_num > task_num * 2 && current_thread_num > min_thread_num)
+        // {
+        //     int dl_thread_num = 
+        // }
     }
     
 }
@@ -168,6 +176,7 @@ int threadPool_Init(thread_pool **pool, int threadNums,int min_thread_num,int ma
     thread_pool * t_pool = (thread_pool *)malloc(sizeof(thread_pool) * 1);
     if (!t_pool)
     {
+        /* 状态码 */
         return MALLOC_ERROR;
     }
     memset(t_pool, 0, sizeof(thread_pool));
@@ -175,9 +184,16 @@ int threadPool_Init(thread_pool **pool, int threadNums,int min_thread_num,int ma
     t_pool->threadId = (pthread_t *)malloc(sizeof(pthread_t) * threadNums);
     memset(t_pool->threadId, 0, sizeof(pthread_t) * threadNums);
 
+    /* 管理者线程 */
+    ret = pthread_create(&(t_pool->manager_thread), NULL, manager_func, t_pool);
+    if (ret == -1)
+    {
+        return THREAD_CREATE_ERROR;
+    }
+
+    /* 循环创建工作线程 */
     for (int idx = 0; idx < threadNums; idx++)
     {
-        /* 线程号 */
         ret = pthread_create(&(t_pool->threadId[idx]), NULL, thread_func, t_pool);
         if (ret == -1)
         {
@@ -190,9 +206,7 @@ int threadPool_Init(thread_pool **pool, int threadNums,int min_thread_num,int ma
     pthread_cond_init(&(t_pool->mutex_cond_ptoc),NULL);
     pthread_cond_init(&(t_pool->mutex_cond_ctop),NULL);
     
-    pthread_create(&(t_pool->thread_manager),NULL,manager_func,t_pool);
-
-
+    
     
 
     /* 使用单向链表实现任务队列 */
@@ -211,11 +225,14 @@ int threadPool_Init(thread_pool **pool, int threadNums,int min_thread_num,int ma
     memset(t_pool->queue, 0, sizeof(t_task));
 #endif
     t_pool->task_size = 0;
-    t_pool->busy_thread = 0;
+    t_pool->busy_thread_num = 0;
     t_pool->max_thread_num = max_thread_num;
     t_pool->min_thread_num = min_thread_num;
     t_pool->current_thread_num = threadNums;
-
+    /* 使能 让线程自己回收 */
+    t_pool->shutdown = 0;       
+    
+    /* 指针赋值 */
     *pool = t_pool;
     return ret;
 }
@@ -253,7 +270,41 @@ int addTask(thread_pool *tpool, void *(*func)(void *), void *arg)
     return ret;
 }
 
+
+/* 销毁线程池 */
 int threadPool_Destory(thread_pool *pool)
+{
+    int ret = 0;
+    ret = threadPool_ResourceFree(pool);
+    ret = threadPool_Free(pool);
+    return ret;
+}
+
+/* 线程池的资源回收 */
+int threadPool_ResourceFree(thread_pool *pool)
+{
+    int ret = 0;
+    pool->shutdown = 1;
+
+    /* 先销毁管理者线程 */
+    pthread_join(pool->manager_thread, NULL);
+
+
+    for (int idx = 0; idx < pool->current_thread_num; idx++)
+    {
+        pthread_cond_broadcast(&(pool->mutex_cond_ptoc));
+    }
+
+    for (int idx = 0; idx < pool->current_thread_num; idx++)
+    {
+        pthread_join(pool->threadId[idx], NULL);
+    }
+
+    return ret;
+}
+
+/* */
+int threadPool_Free(thread_pool *pool)
 {   
     int ret = 0;
 
@@ -265,28 +316,6 @@ int threadPool_Destory(thread_pool *pool)
     }
 
     /* 释放队列 */
-#if 0
-    /* chen */
-    t_task * freeNode = pool->queue->next;
-    while(freeNode != NULL)
-    {
-        pool->queue->next = freeNode->next;
-        free(freeNode);
-        freeNode = pool->queue->next;
-    }
-    free(pool->queue);
-#elif 0
-    /* shuo */
-    t_task *travel = pool->queue;
-    while (pool->queue->next != NULL)
-    {   
-        t_task *freeNode = travel->next;
-        travel = travel->next;
-        free(freeNode);
-    }
-    free(pool->queue);
-#elif 1
-    /* jinyu */
     while(pool->queue->next != NULL)
     {
         t_task *freeNode = pool->queue->next;
@@ -294,7 +323,6 @@ int threadPool_Destory(thread_pool *pool)
         free(freeNode);
     }
     free(pool->queue);
-#endif
 
     /* 释放锁资源 和 条件变量 */
     pthread_mutex_destroy(&(pool->mutex_lock));
